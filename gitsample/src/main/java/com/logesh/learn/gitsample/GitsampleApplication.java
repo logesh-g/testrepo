ab27d8840cbea43299f4acf3012c633dd3ad29e6
Logesh <logeshaec@gmail.com>
2021-11-23 19:23:20 +0530

    initial push

---
+++ gitsample/src/main/java/com/logesh/learn/gitsample/GitsampleApplication.java
@@ -0,0 +1,14 @@
+package com.logesh.learn.gitsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsampleApplication.class, args);
+		System.out.println("Sample application started");
+	}
+
+}
